69910509b77dda66f085904a048e52f14983657f
Stenli777 <alekseysten777@yandex.ru>
2022-09-18 17:53:01 +0300

    edit 2022-09-18

--- controllers/blog.php
+++ controllers/blog.php
@@ -1,18 +1,19 @@
 <?php
 
+namespace Blog;
 
-namespace blog;
-
-class Blog
-{
+class Blog {
     function index(){
         $this->render('blog');
     }
-    function render ($template){
+    function render($template) {
         ob_start();
         include "./view/{$template}.php";
         $content = ob_get_clean();
         $title = 'Каталог';
         include './view/layout/default.php';
+    }
+    function article($id) {
+        include './view/article.php';
     }
 }
--- controllers/catalog.php
+++ controllers/catalog.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace catalog;
+namespace Catalog;
 
 class Catalog {
     function index(){
